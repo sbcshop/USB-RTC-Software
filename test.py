b52e8f44fa512707d6f33c3843d7a2ce79226fb7
SB Components <gajender.singh@sb-components.co.uk>
2021-10-18 12:24:01 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,33 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
 from PyMCP2221A import SMBus
 
 import time
-
-bus = SMBus.SMBus()
-
-# Release RTC 3231
-
-
-# Setup RTC 3231 for temperature reading
-
+ 
 address = 0x68
+register = 0x00
 CONV = 32
 
+
+#sec min hour week day month year
+NowTime = [0x00,0x00,0x18,0x04,0x12,0x08,0x15]  #Edit this line to set RTC Date and Time
+
 w  = ["SUN","Mon","Tues","Wed","Thur","Fri","Sat"];
+
+#/dev/i2c-1
+bus = SMBus.SMBus()
+def ds3231SetTime():
+    #bus.write_i2c_block_data(address,register,NowTime)
+    bus.write_byte_data(0x68, 0x00, NowTime[0]) # set seconds and start clock
+    bus.write_byte_data(0x68, 0x01, NowTime[1]) # set minutes
+    bus.write_byte_data(0x68, 0x02, NowTime[2]) # set hours in 24 hour mode
+    bus.write_byte_data(0x68, 0x03, NowTime[3]) # set day of week
+    bus.write_byte_data(0x68, 0x04, NowTime[4]) # set date
+    bus.write_byte_data(0x68, 0x05, NowTime[5]) # set month
+    bus.write_byte_data(0x68, 0x06, NowTime[6]) # set year - last 2 digits
+ 
+def ds3231ReadTime():
+    return bus.read_i2c_block_data(address,register,7);
 
 # Force a conversion and wait until it completes
 def convTemp(address):
@@ -33,40 +48,20 @@ def getTemp(address):
     tinteger = (byte_tmsb & 0x7f) + ((byte_tmsb & 0x80) >> 7) * -2**8
     tdecimal = (byte_tmsb >> 7) * 2**(-1) + ((byte_tmsb & 0x40) >> 6) * 2**(-2)
     return tinteger + tdecimal
-
-def ReadTime(address):
-    sec = bus.read_byte_data(0x68, 0)
-    min = bus.read_byte_data(0x68, 1)
-    hour = bus.read_byte_data(0x68, 2)
-    day = bus.read_byte_data(0x68, 3)
-    date = bus.read_byte_data(0x68, 4)
-    month = bus.read_byte_data(0x68, 5)
-    year = bus.read_byte_data(0x68, 6)
-    return (hour,min,sec,date,month,year, day)
-
-def SetTime(address):
-    bus.write_byte_data(0x68, 0x00, 0x00) # set seconds and start clock
-    bus.write_byte_data(0x68, 0x01, 0x38) # set minutes
-    bus.write_byte_data(0x68, 0x02, 0x13) # set hours in 24 hour mode
-    bus.write_byte_data(0x68, 0x03, 0x04) # set day of week
-    bus.write_byte_data(0x68, 0x04, 0x07) # set date
-    bus.write_byte_data(0x68, 0x05, 0x03) # set month
-    bus.write_byte_data(0x68, 0x06, 0x15) # set year - last 2 digits
-    
-SetTime(address)
-while True:
-    t=ReadTime(address)
-    hour1=t[0]&0x3F
-    min1=t[1]&0x7F
-    sec1=t[2]&0x7F
-    date1=t[3]
-    month1=t[4]&0x1F
-    year1=t[5]
-    day1=t[6]&0x3F
-    print(t)
-    print("%s/%s/%s %s:%s:%s %s" %(date1,month1,year1,hour1,min1,sec1,w[day1-1]))
+ 
+ds3231SetTime()
+while 1:
+    t = ds3231ReadTime()
+    t[0] = t[0]&0x7F  #sec
+    t[1] = t[1]&0x7F  #min
+    t[2] = t[2]&0x3F  #hour
+    t[3] = t[3]&0x07  #week
+    t[4] = t[4]&0x3F  #day
+    t[5] = t[5]&0x1F  #month
+    print("20%x/%x/%x %x:%x:%x  %s" %(t[6],t[5],t[4],t[2],t[1],t[0],w[t[3]-1]))
+    time.sleep(1)
 
     '''Celsius = getTemp(address)
     Fahrenheit = 9.0/5.0 * Celsius + 32
-    print (Fahrenheit, "*F /", Celsius, "*C")'''
-    time.sleep(1)
+    print (Fahrenheit, "*F /", Celsius, "*C")
+    time.sleep(1)'''
